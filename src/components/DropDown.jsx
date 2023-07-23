8e6e1c4988a515bec07c434f96902c21a738d36a
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-07-23 19:54:10 +0800

    Added dropdown menu

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,14 +1,22 @@
-function DropDown({ icon, label, classname }) {
+
+function Dropdown({ items, icon, label, classname }) {
   return (
-    <div>
-      <div>
-        <a href="">{label}</a>
-        <img src={icon} width={15} className={classname}/>
-      </div>
+    <div className="dropdown">
+      <a href="">{label}</a>
+      <img src={icon} width={15} className={classname}/>
       <div className="dropdown-menu">
-                
+        {
+          items.map(item => (
+              <div className="dropdown-menu-item">
+                <div>
+                  <img src={item.iconName} alt="" />
+                </div>
+                <div>{item.name}</div>
+              </div>
+            ))
+        }
       </div>
     </div>
   );
 }
-export default DropDown;
+export default Dropdown;

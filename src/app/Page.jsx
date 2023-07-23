49f34b45cd96ccca5425867afa07017f4df06578
Rhea De Ocampo <rheaaadeocampo@gmail.com>
2023-07-23 21:58:53 +0800

    feature-footage done

--- src/app/Page.jsx
+++ src/app/Page.jsx
@@ -4,15 +4,20 @@ import BlogList from "../shared/blog-list/Index";
 import SubscribeUs from "../components/SubscribeUs";
 import notificationGif from '../assets/images/notification.gif'
 import envelope from '../assets/svgs/envelope.svg'
+import Footer from "../shared/footer/Index";
 function Page(){
     // const navigate = useNavigate();
     // return <div onClick={() => navigate('/aboutus')}>HELLO PINAKAMAMAHAL KO</div>
     return(
+        <>
         <div className="bg">
            <Header />
            <BlogList />
            <SubscribeUs icon={notificationGif} envelopeicon={envelope}/>
+           <Footer />
         </div >
+        
+        </>
     )
 }
 
---
+++ src/shared/footer/Index.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return(
+    <>
+    <div className="footer-con">
+       <h2>@2023 Productopedia.tech</h2>
+       <div className="right-footer">
+        <a href="">About us</a>
+        <a href="">Privacy Policy</a>
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer;

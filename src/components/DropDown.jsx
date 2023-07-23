04b2125d20088d1ab93de8b638feaa3e5dc030ee
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-07-23 22:16:05 +0800

    Show dropdown menu on hover

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,21 +1,25 @@
+import { Link } from "react-router-dom";
 
 function Dropdown({ items, icon, label, classname }) {
+
   return (
     <div className="dropdown">
-      <a href="">{label}</a>
-      <img src={icon} width={15} className={classname}/>
-      <div className="dropdown-menu">
-        {
-          items.map(item => (
-              <div className="dropdown-menu-item">
-                <div>
-                  <img src={item.iconName} alt="" />
-                </div>
-                <div>{item.name}</div>
-              </div>
-            ))
-        }
+      <div>
+        <a href="">{label}</a>
+        <img src={icon} width={15} className={classname}/>
       </div>
+      <div className="dropdown-menu">
+          {
+            items.map(item => (
+                <Link className="dropdown-menu-item" to='/'>
+                  <div>
+                    <img src={item.iconName} alt="" />
+                  </div>
+                  <div>{item.name}</div>
+                </Link>
+              ))
+          }
+        </div>
     </div>
   );
 }

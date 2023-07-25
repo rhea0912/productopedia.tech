26b9de8c6682356d325ad5b7dccee0ffb4acb5e2
Rhea De Ocampo <rheaaadeocampo@gmail.com>
2023-07-25 21:26:39 +0800

    Responsive design

--- src/shared/header/TopHeader.jsx
+++ src/shared/header/TopHeader.jsx
@@ -1,5 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSearch} from "@fortawesome/free-solid-svg-icons";
+import menubar from '../../assets/svgs/menu.svg'
+import search from '../../assets/svgs/search.svg'
+import arrowmenu from '../../assets/svgs/arrowmenu.svg'
 function TopHeader(){
 return (
     <>
@@ -8,8 +11,12 @@ return (
                 <button type="button" className="searchBtn"> <FontAwesomeIcon icon={faSearch} className="search-icon" title="search"/></button>
                 <button className="subscribe">SUBSCRIBE</button>
     </div>
+    <label id="check">
+            <img src={search} className="search"/>
+            <img src={menubar} className="menubar" onClick={arrowmenu}/>
+            <img src={arrowmenu} className="arrowmenu"/>
+    </label>
     </>
 )
 }
-
 export default TopHeader;
--- src/shared/header/index.jsx
+++ src/shared/header/index.jsx
@@ -4,7 +4,6 @@ import BottomHeader from "./BottomHeader";
 function Header(){
    return(
     <div>
-
         <div className="top-header-con container">
             <h1>Productopedia.tech</h1>
             <TopHeader />
